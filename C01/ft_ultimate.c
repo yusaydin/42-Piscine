6cad3a1d80fab8fa3bc6349fe2987b961094d261
yusaydin <62809879+yusaydin@users.noreply.github.com>
2023-09-11 11:10:31 +0300

    Update ft_ultimate.c

--- C01/ft_ultimate.c
+++ C01/ft_ultimate.c
@@ -8,31 +8,29 @@ void	ft_ultimate_ft(int *********nbr)
 int	main()
 {
 	int n;
-	int *nbr8;
-	int **nbr7;
-	int ***nbr6;
-	int ****nbr5;
-	int *****nbr4;
-	int ******nbr3;
-	int *******nbr2;
-	int ********nbr1;
-	int *********nbr;
+	int *ptr1;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
 
-
-	n = 21;
+	n=21;
 	
-	nbr8 = &n;
-	nbr7 = &nbr8;
-	nbr6 = &nbr7;
-	nbr5 = &nbr6;
-	nbr4 = &nbr5;
-	nbr3 = &nbr4;
-	nbr2 = &nbr3;
-	nbr1 = &nbr2;
-	nbr = &nbr1;
-	
+	ptr1 = &n;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
 
-	ft_ultimate_ft(nbr);
-	printf("%d", n);	
+	ft_ultimate_ft(ptr9);
+	printf("%d  ", n);	
 	return 0;
 }
